f5a6cc29b4b62bbdb7eba6e34b3acfd82414e83f
Mortadha Boubaker <mortadhaboubaker12@gmail.com>
2022-05-11 00:27:29 -1200

    create project react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Task from "./pages/home/Task";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Header from "./component/header/Header";
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import { BrowserRouter as Router } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
